c76dabbda7998d7df902b07d1c91aabfcc37f876
daniel <yoojeenkim@gmail.com>
2021-11-23 16:23:26 -0500

    added redux packages

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,11 +8,13 @@ import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
